38197a276c3f1d98724e1708f113fac3b34daa52
Aymane Harmaz <aymane.harmaz1@gmail.com>
2024-05-25 18:06:36 +0100

    adding domain entity, and repository class

--- src/main/java/ma/demo/liquibase/LiquibaseDemoApplication.java
+++ src/main/java/ma/demo/liquibase/LiquibaseDemoApplication.java
@@ -1,11 +1,27 @@
 package ma.demo.liquibase;
 
+import ma.demo.liquibase.repositories.BookRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LiquibaseDemoApplication {
+public class LiquibaseDemoApplication implements CommandLineRunner {
+    private final BookRepository bookRepository;
+
+    public LiquibaseDemoApplication(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(LiquibaseDemoApplication.class);
+    }
+
+    @Override
+    public void run(String... args) {
+        bookRepository.findAll()
+                .forEach(book -> {
+                    System.out.println(book.toString());
+                });
     }
 }
---
+++ src/main/java/ma/demo/liquibase/domain/Book.java
@@ -0,0 +1,23 @@
+package ma.demo.liquibase.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "books")
+public class Book {
+    @Id
+    private String isbn;
+    @Column
+    private String title;
+}
